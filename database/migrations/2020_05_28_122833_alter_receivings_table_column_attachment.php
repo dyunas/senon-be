3cf71d7f326d75441e7f754f42769a556b9a91e4
dyunas <jonathan.almodiel.quebral@gmail.com>
2020-05-28 14:51:36 +0800

    - renamed class name to AlterReceivingsTableColumnAttachment

--- database/migrations/2020_05_28_122833_alter_receivings_table_column_attachment.php
+++ database/migrations/2020_05_28_122833_alter_receivings_table_column_attachment.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterAssignmentTableColumnAttachment extends Migration
+class AlterReceivingsTableColumnAttachment extends Migration
 {
   /**
    * Run the migrations.
